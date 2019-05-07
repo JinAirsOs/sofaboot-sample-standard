61470f37ee331309edbf0a6c3882d393dc3dfe12
肃羽 <kerry.pzd@antfin.com>
2019-05-07 18:34:05 +0800

    add studentrpcservice zookeeper

--- app/web/src/main/java/com/alipay/sofa/SOFABootWebApplication.java
+++ app/web/src/main/java/com/alipay/sofa/SOFABootWebApplication.java
@@ -17,12 +17,16 @@
 package com.alipay.sofa;
 
 import com.alipay.sofa.facade.StudentRpcService;
+import com.alipay.sofa.rpc.common.RpcConstants;
 import com.alipay.sofa.rpc.config.ConsumerConfig;
+import com.alipay.sofa.rpc.config.RegistryConfig;
+import com.alipay.sofa.runtime.api.annotation.SofaReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * SOFABootWebApplication
@@ -32,9 +36,24 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @EnableScheduling
 @org.springframework.boot.autoconfigure.SpringBootApplication
+@ImportResource({ "rpc-client.xml" })
 public class SOFABootWebApplication {
 
     public static void main(String[] args) throws Exception {
         ApplicationContext applicationContext = SpringApplication.run(SOFABootWebApplication.class, args);
+        //zookeeper 注册中心获取service，其余应用可以这样调用StudentRpcService
+        /*RegistryConfig registryConfig = new RegistryConfig()
+                .setProtocol(RpcConstants.REGISTRY_PROTOCOL_ZK)
+                .setAddress("127.0.0.1:2181");//zookeeper地址
+
+        ConsumerConfig<StudentRpcService> consumerConfig = new ConsumerConfig<StudentRpcService>()
+                .setInterfaceId(StudentRpcService.class.getName())
+                .setRegistry(registryConfig)
+                .setTimeout(3000);
+
+        StudentRpcService studentRpcService = consumerConfig.refer();*/
+        //zookeeper注册中心获取bean。
+        /*StudentRpcService studentRpcService = (StudentRpcService) applicationContext.getBean("studentRpcService");
+        System.out.println(studentRpcService.sayName());*/
     }
 }
--- app/web/src/main/java/com/alipay/sofa/scheduler/SampleScheduler.java
+++ app/web/src/main/java/com/alipay/sofa/scheduler/SampleScheduler.java
@@ -2,31 +2,29 @@ package com.alipay.sofa.scheduler;
 
 import com.alipay.sofa.facade.StudentRpcService;
 import com.alipay.sofa.rpc.config.ConsumerConfig;
+import com.alipay.sofa.runtime.api.annotation.SofaReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Component
 public class SampleScheduler {
-    //5秒
-    private static final long FIVE_SECONDS = 5000;
+    //20秒
+    private static final long TWENTY_SECONDS = 20000;
     private static final Logger LOGGER = LoggerFactory.getLogger(SampleScheduler.class);
 
-    @Scheduled(fixedRate = FIVE_SECONDS)
+    @Resource
+    private StudentRpcService studentRpcService;
+
+    @Scheduled(fixedRate = TWENTY_SECONDS)
     public void scheduledTask() {
-
-        ConsumerConfig<StudentRpcService> consumerConfig = new ConsumerConfig<StudentRpcService>()
-                .setInterfaceId(StudentRpcService.class.getName()) // 指定接口
-                .setProtocol("bolt") // 指定协议
-                .setDirectUrl("bolt://127.0.0.1:12200") // 指定直连地址
-                .setConnectTimeout(10 * 1000);
-
-        StudentRpcService studentRpcService = consumerConfig.refer();
-
+        //注意 studentRpcService已经被zookeeper注册成为bean
         LOGGER.info(studentRpcService.sayName());
     }
 }
