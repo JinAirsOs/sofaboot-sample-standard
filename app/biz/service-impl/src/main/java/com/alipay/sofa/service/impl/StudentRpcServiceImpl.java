4f28b9c0ff80b0c1e3ccc2b75d54a7f9d6fcca6c
肃羽 <kerry.pzd@antfin.com>
2019-05-27 17:08:13 +0800

    fix

--- app/biz/service-impl/src/main/java/com/alipay/sofa/service/impl/StudentRpcServiceImpl.java
+++ app/biz/service-impl/src/main/java/com/alipay/sofa/service/impl/StudentRpcServiceImpl.java
@@ -5,6 +5,8 @@ import com.alipay.sofa.common.dal.tables.Student;
 import com.alipay.sofa.facade.StudentRpcService;
 import com.alipay.sofa.runtime.api.annotation.SofaService;
 import com.alipay.sofa.runtime.api.annotation.SofaServiceBinding;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -16,18 +18,21 @@ import java.util.Map;
 //用@Service注册bean
 //用@SofaService(bindings= {@SofaServiceBinding(bindingType = "bolt")})注册rpc服务
 //也可以在xml中设置，便于理解。
-@Service
-@SofaService(bindings= {@SofaServiceBinding(bindingType = "bolt"),@SofaServiceBinding(bindingType = "rest")})
+/*@Service
+@SofaService(bindings= {@SofaServiceBinding(bindingType = "bolt"),@SofaServiceBinding(bindingType = "rest")})*/
 public class StudentRpcServiceImpl implements StudentRpcService {
 
+    private static final Logger logger = LoggerFactory.getLogger(StudentRpcServiceImpl.class);
     @Resource
     private StudentDAO studentDAO;
 
     public String ping(){
+        logger.info("ping() is called");
         return "pong";
     }
 
     public String getStudentNameById(int id){
+        logger.info("getStudentNameById(int id) is called");
         Optional<Student> studentOption = studentDAO.findById(id);
         if(studentOption.isPresent()){
             return studentOption.get().getName();
@@ -37,6 +42,7 @@ public class StudentRpcServiceImpl implements StudentRpcService {
     }
 
     public Map<String,Student> getStudentInfoById(int id) {
+        logger.info("getStudentInfoById(int id) is called");
         Optional<Student> studentOption = studentDAO.findById(id);
         Map<String,Student> studentInfo = new HashMap<>();
         if(studentOption.isPresent()){
